2e40447c2b0f31ab4d304baf0024eddac62bb77e
Jordon-py <c.jordon@icloud.com>
2025-02-24 10:01:55 -0800

    First name state implimented in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 // src/App.jsx
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [title, setTitle] = useState('The full name will appear here.');
+  const [firstName, setFirstName] = useState('');
+
+  // when invoked, update the firstName state to equal the new value 
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <form>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          id="firstName"
+          name="firstName"
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </form>
+    </>
+  );
 };
 
-export default App;
